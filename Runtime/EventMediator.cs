b98e169131675653b77c4ea41a1f6ff8d9941dd8
Dor Ben Dor <agesonera@gmail.com>
2023-08-20 09:58:55 +0300

    [tool] added the event mediator tool

---
+++ Runtime/EventMediator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBD.UnityCodingTools
+{
+    public class EventMediator
+    {
+        private static EventMediator _instance;
+        public static EventMediator Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new EventMediator();
+
+                return _instance;
+            }
+            private set => _instance = value;
+        }
+
+        private Dictionary<Type, EventHelper> _eventHelpers = new Dictionary<Type, EventHelper>();
+
+        private EventMediator()
+        {
+            if (_instance != null)
+                throw new Exception("Event Mediator: Cannot create multiple instances of EventMediator");
+
+            _instance = this;
+        }
+
+        public void Publish<T>(string eventAction, T args)
+        {
+            if (!_eventHelpers.ContainsKey(typeof(T)))
+                _eventHelpers.Add(typeof(T), new EventHelper<T>());
+
+            var helper = _eventHelpers[typeof(T)] as EventHelper<T>;
+
+            helper.Publish(eventAction, args);
+        }
+
+        public void Subscribe<T>(string eventAction, Action<T> action)
+        {
+            if (!_eventHelpers.ContainsKey(typeof(T)))
+                _eventHelpers.Add(typeof(T), new EventHelper<T>());
+
+            var helper = _eventHelpers[typeof(T)] as EventHelper<T>;
+
+            helper.Subscribe(eventAction, action);
+        }
+
+        public void Unsubscribe<T>(string eventAction, Action<T> action)
+        {
+            if (!_eventHelpers.ContainsKey(typeof(T)))
+                _eventHelpers.Add(typeof(T), new EventHelper<T>());
+
+            var helper = _eventHelpers[typeof(T)] as EventHelper<T>;
+
+            helper.Unsubscribe(eventAction, action);
+        }
+
+        private abstract class EventHelper { }
+
+        private class EventHelper<T> : EventHelper
+        {
+            private Dictionary<string, Action<T>> _subscribers;
+
+            public EventHelper()
+            {
+                _subscribers = new Dictionary<string, Action<T>>();
+            }
+
+            public void Publish(string eventAction, T args)
+            {
+
+                if (!_subscribers.ContainsKey(eventAction))
+                    return;
+                if (_subscribers[eventAction] == null)
+                    return;
+
+                _subscribers[eventAction].Invoke(args);
+            }
+
+            public void Subscribe(string eventAction, Action<T> action)
+            {
+                if (!_subscribers.ContainsKey(eventAction))
+                {
+                    _subscribers[eventAction] = new Action<T>(action);
+                    return;
+                }
+
+                _subscribers[eventAction] += action;
+            }
+
+            public void Unsubscribe(string eventAction, Action<T> action)
+            {
+                if (!_subscribers.ContainsKey(eventAction))
+                    return;
+
+                _subscribers[eventAction] -= action;
+            }
+        }
+    }
+}
